4836f09406ffb56b8a3e5e298b4be3aac01af0f2
lcahlander <loren.cahlander@gmail.com>
2020-07-15 19:35:58 -0400

    Adding router to the app

--- src/main/js/frontend/src/App.js
+++ src/main/js/frontend/src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">My React Application</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
+        <Router>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">My React Application</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/features">Features</Nav.Link>
+                    <Nav.Link href="/pricing">Pricing</Nav.Link>
+                </Nav>
+            </Navbar>
+            <Switch>
+                <Route path="/features">
+                    <div>
+                        <h1>Features</h1>
+                    </div>
+                </Route>
+                <Route path="/pricing">
+                    <div>
+                        <h1>Pricing</h1>
+                    </div>
+                </Route>
+                <Route path="/">
+                    <div>
+                        <h1>Home</h1>
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
   );
 }
 
